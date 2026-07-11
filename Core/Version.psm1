e14c783dfd4d34dc2f5adabc210e3413c523de0f
Hayden <h4xwbnbzpc@privaterelay.appleid.com>
2026-07-10 20:43:57 -0500

    Update toolkit version to v0.3.0

--- Core/Version.psm1
+++ Core/Version.psm1
@@ -1,4 +1,4 @@
-$Global:ToolkitVersion = "0.2.0"
+$Global:ToolkitVersion = "0.3.0"
 
 function Get-ToolkitVersion {
     return $Global:ToolkitVersion
---
+++ Tests/Version.Tests.ps1
@@ -0,0 +1,21 @@
+Describe "Version Module" {
+
+    BeforeAll {
+        $Root = Split-Path -Parent $PSScriptRoot
+        Import-Module "$Root\Core\Version.psm1" -Force
+    }
+
+    It "imports successfully" {
+        {
+            Import-Module "$Root\Core\Version.psm1" -Force
+        } | Should -Not -Throw
+    }
+
+    It "returns the current release version" {
+        Get-ToolkitVersion | Should -Be "0.3.0"
+    }
+
+    It "stores the version globally for the console header" {
+        $Global:ToolkitVersion | Should -Be "0.3.0"
+    }
+}
